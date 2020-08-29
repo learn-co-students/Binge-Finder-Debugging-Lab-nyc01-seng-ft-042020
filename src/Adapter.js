0107f6795afcd98e2c2504f3b1ba3f9857d8ba0e
Jessica <github email address>
2020-08-29 09:12:10 -0400

    Done.

--- src/Adapter.js
+++ src/Adapter.js
@@ -2,12 +2,12 @@ class Adapter {
   static getShows (){
     fetch("http://api.tvmaze.com/shows")
     .then(res => res.json())
-  }
+  };
 
-  // static getShowEpisodes (showID){
-  //   return fetch(`http://api.tvmaze.com/shows/${showID}/episodes`)
-  //   .then(res => res.json)
-  // }
-}
+  static getShowEpisodes (showID){
+    return fetch(`http://api.tvmaze.com/shows/${showID}/episodes`)
+    .then(res => res.json)
+  };
+};
 
-export default Adapter
+export default Adapter;
